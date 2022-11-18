8141cb05f124234a040e73cfb6d6aef065c2d654
Michael Sylvester <m.sylvester18@yahoo.com>
2022-11-17 22:48:32 -0600

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,15 @@
+let x = 5
+let y = 5
+
+
+// Write your code below this line.
+
+if(y>x){
+    console.log(y)
+} else if (y===x){
+    console.log("the values are identical")
+} else {
+    console.log(x)
+}
+
+
